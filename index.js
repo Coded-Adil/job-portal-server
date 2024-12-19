8d2b33f0165ede329a7cb01c7501a4db45c2db09
Adil Rahman <itsadil2022@gmail.com>
2024-12-19 23:56:29 +0600

    Adding job application api

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ async function run() {
 
     // Job related apis
     const jobsCollection = client.db('jobPortal').collection('jobs');
-
+    const jobApplicationCollection = client.db('jobPortal').collection('job_applications');
     app.get('/jobs', async(req, res) => {
       const cursor = jobsCollection.find();
       const result = await cursor.toArray();
@@ -40,6 +40,13 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id)};
       const result = await jobsCollection.findOne(query);
+      res.send(result);
+    })
+
+    // job applicaton apis
+    app.post('/job-applications', async(req, res) => {
+      const application = req.body;
+      const result = await jobApplicationCollection.insertOne(application);
       res.send(result);
     })
 
